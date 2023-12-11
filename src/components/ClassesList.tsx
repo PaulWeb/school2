5d26e11bb0176b8e70bdb10ba2797fd7c5f92b6d
AnatBCode <AnatBCode@gmail.com>
2023-12-10 20:31:14 -0500

    Added class dialog

--- src/components/ClassesList.tsx
+++ src/components/ClassesList.tsx
@@ -1,13 +1,7 @@
 import React, { FC } from 'react';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { classes } from '../db';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
+import TextField from '@mui/material/TextField';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 import Dialog from '@mui/material/Dialog';
@@ -17,7 +11,8 @@ import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 
 import ListItemText from '@mui/material/ListItemText';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
+import EditClassDialog from './EditClassDialog';
 import supabase, { supabaseKey, supabaseUrl } from '../supabase'
 //import format from 'date-fns/format';
 //import { List, ListItem } from '@material-ui/core';
@@ -32,46 +27,14 @@ interface EditRowModalProps {
     rowData: any; // Adjust the type according to your data structure
     onSave: () => void;
     onClose: () => void;
-  }
-
-  const EditRowModal: FC<EditRowModalProps> = ({ rowData, onSave, onClose }) => {
-    // Your modal content and form for editing the row data
-  
-    const handleEntered = () => {
-        // The `autoFocus` is not used because, if used, the same Enter that saves
-        // the cell triggers "No". Instead, we manually focus the "No" button once
-        // the dialog is fully open.
-        // noButtonRef.current?.focus();
-      };
-      const noButtonRef = React.useRef<HTMLButtonElement>(null);      
-
-    return (
-
-
-            <Dialog
-              maxWidth="xs"
-              TransitionProps={{ onEntered: handleEntered }}
-              open={true}
-            >
-              <DialogTitle>Are you sure?</DialogTitle>
-              <DialogContent dividers>
-                {`Pressing 'Yes' will change $ {mutation}.`}
-              </DialogContent>
-              <DialogActions>
-                <Button ref={noButtonRef} onClick={onClose}>
-                  Close
-                </Button>
-                <Button onClick={onSave}>Save</Button>
-              </DialogActions>
-            </Dialog>
-    );
-  };
+}
 
 const getClassesQuery = `
   query GetClasses {
     fs_classesCollection {
         edges {
           node {
+            nodeId
             id
             name
             duration
@@ -85,13 +48,13 @@ const getClassesQuery = `
 `;
 
 
- 
+
 const ClassesList: React.FC = () => {
 
     const columns: GridColDef[] = [
         { field: 'id', headerName: 'ID', width: 70 },
         { field: 'name', headerName: 'Class Name', width: 230 },
-        { field: 'duration', headerName: 'Duration', type: 'number',  width: 90 },
+        { field: 'duration', headerName: 'Duration', type: 'number', width: 90 },
         { field: 'code', headerName: 'Code', width: 100 },
         { field: 'eventType', headerName: 'Event Type', width: 120 },
         {
@@ -111,7 +74,7 @@ const ClassesList: React.FC = () => {
                 );
             },
         },
-      ];
+    ];
 
     const [classes, setClasses] = useState<any[]>([]);
     const [editedRow, setEditedRow] = useState(null);
@@ -120,78 +83,79 @@ const ClassesList: React.FC = () => {
     const handleEditClick = (row: any) => {
         setEditedRow(row);
         setIsModalOpen(true);
-      };
+    };
 
-      const handleSave = () => {
+    const handleSave = () => {
         // Handle saving the edited row data
         setIsModalOpen(false);
-      };
-    
-      const handleClose = () => {
+    };
+
+    const handleClose = () => {
         setIsModalOpen(false);
-      };      
+    };
 
-  
+
     useMemo(async () => {
-         const {
+        const {
             data: { session },
-          } = await supabase.auth.getSession() 
-    
-      const body = await fetch(`${supabaseUrl}/graphql/v1`, { 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          apikey: supabaseKey,
-          Authorization: `Bearer ${session?.access_token ?? supabaseKey}`,
-        },
-        body: JSON.stringify({ query: getClassesQuery }),
-      });
+        } = await supabase.auth.getSession()
 
-      const { data: { fs_classesCollection: { edges }, }, } = await body.json();
+        const body = await fetch(`${supabaseUrl}/graphql/v1`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                apikey: supabaseKey,
+                Authorization: `Bearer ${session?.access_token ?? supabaseKey}`,
+            },
+            body: JSON.stringify({ query: getClassesQuery }),
+        });
+
+        const { data: { fs_classesCollection: { edges }, }, } = await body.json();
 
 
-      setClasses(edges.map((item: any) => {
-        const subsetItem = { 
-            id: item.node.id, 
-            name: item.node.name, 
-            duration: item.node.duration, 
-            code: item.node.code, 
-            eventType: item.node.fs_eventtype.typename, 
-            productCategory: item.node.fs_prodcategory.catname, 
-        };
-        return subsetItem;
-      })
-);
+        setClasses(edges.map((item: any) => {
+            const subsetItem = {
+                nodeId: item.node.nodeId,
+                id: item.node.id,
+                name: item.node.name,
+                duration: item.node.duration,
+                code: item.node.code,
+                eventType: item.node.fs_eventtype.typename,
+                productCategory: item.node.fs_prodcategory.catname,
+            };
+            return subsetItem;
+        })
+        );
 
 
     }, []);
 
     return (
-    <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-    >
-    <Toolbar />
+        <Box
+            component="main"
+            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+        >
+            <Toolbar />
 
-    <div style={{ height: 900, width: '100%' }}>
-      <DataGrid
-        rows={classes}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 15 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        //checkboxSelection
-      />
-      {isModalOpen && (
-        <EditRowModal rowData={editedRow} onSave={handleSave} onClose={() => setIsModalOpen(false)} />
-      )}
+            <div style={{ height: 900, width: '100%' }}>
+                <DataGrid
+                    rows={classes}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 15 },
+                        },
+                    }}
+                    pageSizeOptions={[5, 10]}
+                //checkboxSelection
+                />
+                {isModalOpen && (
+                    <EditClassDialog rowData={editedRow} onSave={handleSave} onClose={() => setIsModalOpen(false)} />
+                )}
 
 
-    </div>
-    </Box>
+            </div>
+        </Box>
     );
 }
 
---
+++ src/components/EditClassDialog.tsx
@@ -0,0 +1,159 @@
+import React, { FC } from 'react';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import TextField from '@mui/material/TextField';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+
+import ListItemText from '@mui/material/ListItemText';
+import { useState, useMemo, useEffect } from 'react';
+import supabase, { supabaseKey, supabaseUrl } from '../supabase'
+//import format from 'date-fns/format';
+//import { List, ListItem } from '@material-ui/core';
+//import styled from 'styled-components';
+//import { useCallback } from 'react';
+//import { History } from 'history';
+//import { useChatsQuery } from '../../graphql/types';
+//import { useGetChatPrefetch } from '../ChatRoomScreen';
+
+
+interface EditClassDialogProps {
+    rowData: any; // Adjust the type according to your data structure
+    //render: (open: any) => any;
+    onSave: () => void;
+    onClose: () => void;
+}
+
+const EditClassDialog: FC<EditClassDialogProps> = ({ rowData, onSave, onClose }) => {
+    // Your modal content and form for editing the row data
+
+    const getClassDataQuery = `
+    query GetClass($nid: ID!)  { 
+        node (nodeId: $nid)
+         { 
+              ... on fs_classes 
+          { 
+            nodeId
+            id 
+            name 
+            description
+            active
+            category
+            eventtype
+            notificationmsg
+            followupmsg
+            signupmsg
+            duration
+            code
+            signcutoff
+            cancelcutoff
+            wlcutoff
+            listorder
+          }
+        }
+      }
+  `;
+
+    const [classData, setClassData] = useState<any>({});
+
+/*     useEffect(() => {
+//        const {
+//            data: { session },
+        //} = supabase.auth.getSession()
+
+        const nid = rowData.nodeId;
+        fetch(`${supabaseUrl}/graphql/v1`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                apikey: supabaseKey,
+                Authorization: `Bearer ${supabaseKey}`,
+            },
+            
+            body: JSON.stringify({ query: getClassDataQuery,  variables : {nid}}),
+        })
+        .then(response => response.json())
+        .then(function(data){
+            console.log(data);
+            setClassData(data.data.node);
+        var a = 0;    
+        });
+
+        }, 
+        [rowData.code]); */
+
+
+        useMemo(async () => {
+            const {
+                data: { session },
+            } = await supabase.auth.getSession()
+
+            const nid = rowData.nodeId;
+    
+            const body = await fetch(`${supabaseUrl}/graphql/v1`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    apikey: supabaseKey,
+                    Authorization: `Bearer ${session?.access_token ?? supabaseKey}`,
+                },
+                body: JSON.stringify({ query: getClassDataQuery, variables: {nid} }),
+            });
+    
+            const cData = await body.json();
+    
+  
+            setClassData(cData.data.node);
+        }, [rowData.nodeId]);
+
+
+    const handleEntered = () => {
+        // The `autoFocus` is not used because, if used, the same Enter that saves
+        // the cell triggers "No". Instead, we manually focus the "No" button once
+        // the dialog is fully open.
+        // noButtonRef.current?.focus();
+    };
+    const noButtonRef = React.useRef<HTMLButtonElement>(null);
+
+    return (
+        <Dialog
+            maxWidth="xs"
+            TransitionProps={{ onEntered: handleEntered }}
+            open={true}
+        >
+            <DialogTitle>Are you sure?</DialogTitle>
+            <DialogContent dividers>
+                {classData?.nodeId ?? ""} <br/>
+                {classData?.id ?? ""} <br/>
+                {classData?.name ?? ""} <br/>
+                {classData?.description ?? ""}<br/>
+                {classData?.active ?? ""} <br/>
+                {classData?.category ?? ""}<br/>
+                {classData?.eventtype ?? ""} <br/>
+                {classData?.notificationmsg ?? ""}<br/>
+                {classData?.followupmsg ?? ""} <br/>
+                {classData?.signupmsg ?? ""}<br/>
+                {classData?.duration ?? ""} <br/>
+                {classData?.code ?? ""}<br/>
+                {classData?.signcutoff ?? ""} <br/>
+                {classData?.cancelcutoff ?? ""}<br/>
+                {classData?.wlcutoff ?? ""} <br/>
+                {classData?.listorder ?? ""}<br/>
+            </DialogContent>
+            <DialogActions>
+                <Button ref={noButtonRef} onClick={onClose}>
+                    Close
+                </Button>
+                <Button onClick={onSave}>Save</Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+
+export default EditClassDialog;
